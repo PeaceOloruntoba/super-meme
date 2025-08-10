d8087d7a21c2c60e16619ea86c68cc6c33be8bd2
PeaceOloruntoba <profprincepeace@gmail.com>
2025-08-10 16:55:05 +0100

    pss

---
+++ src/v1/routes/projects.routes.js
@@ -0,0 +1,33 @@
+import express from "express";
+import methodNotAllowed from "../../middlewares/methodNotAllowed.js";
+import { isAuth } from "../../middlewares/auth.js";
+import {
+  createProject,
+  getAllProjects,
+  getAllProjectsByClient,
+  getSingleProject,
+  updateProject,
+  deleteProject,
+} from "../controllers/projects.controller.js";
+
+const router = express.Router();
+
+router
+  .route("/")
+  .post(isAuth, createProject)
+  .get(isAuth, getAllProjects)
+  .all(methodNotAllowed);
+
+router
+  .route("/:projectId")
+  .get(isAuth, getSingleProject)
+  .patch(isAuth, updateProject)
+  .delete(isAuth, deleteProject)
+  .all(methodNotAllowed);
+
+clientNestedRouter
+  .route("/")
+  .get(isAuth, getAllProjectsByClient)
+  .all(methodNotAllowed);
+
+export default router;
