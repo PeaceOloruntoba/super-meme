7a56f51daffa1fcf2c81360dc5b73b004845f286
PeaceOloruntoba <profprincepeace@gmail.com>
2025-08-12 22:25:40 +0100

    Create subscription.model.js

---
+++ src/v1/models/subscription.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const SubscriptionSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    planId: {
+      type: String,
+      enum: ["premium", "enterprise"],
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["trialing", "active", "cancelled", "overdue"],
+      default: "trialing",
+    },
+    stripeCustomerId: String,
+    stripeSubscriptionId: String,
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    dueDate: {
+      type: Date,
+      required: true,
+    },
+    paymentMethod: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Subscription", SubscriptionSchema);
