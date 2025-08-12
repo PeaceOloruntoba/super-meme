3c8613ca82d5a137d5d54b081464a2e276890709
PeaceOloruntoba <profprincepeace@gmail.com>
2025-08-12 22:21:31 +0100

    qppp

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import fileUpload from "express-fileupload";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
+import cron from "node-cron";
+import Stripe from "stripe";
+
 dotenv.config();
 
 import connectDB from "./src/config/connectDB.js";
@@ -17,11 +20,16 @@ import measurementsRoutesV1 from "./src/v1/routes/measurements.routes.js";
 import projectsRoutesV1 from "./src/v1/routes/projects.routes.js";
 import invoiceRoutesV1 from "./src/v1/routes/invoice.routes.js";
 import patternRoutesV1 from "./src/v1/routes/pattern.routes.js";
+import subscriptionRoutesV1 from "./src/v1/routes/subscription.routes.js";
+import webhookRoutesV1 from "./src/v1/routes/webhook.routes.js";
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.json());
+app.use("/webhook/stripe", webhookRoutesV1);
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cors());
 app.use(
   fileUpload({
@@ -39,12 +47,17 @@ app.use("/api/v1/measurements", measurementsRoutesV1);
 app.use("/api/v1/projects", projectsRoutesV1);
 app.use("/api/v1/invoice", invoiceRoutesV1);
 app.use("/api/v1/pattern", patternRoutesV1);
+app.use("/api/v1/subscription", subscriptionRoutesV1);
 // app.use("/api/v1/admin", adminRoutes);
 app.use(notFound);
 app.use(errorMiddleware);
 
-
-
+cron.schedule("0 2 * * *", async () => {
+  console.log(
+    "Running scheduled job: Checking and deactivating expired subscriptions..."
+  );
+  await subscriptionService.checkAndDeactivateExpiredSubscriptions();
+});
 
 const startServer = async () => {
   try {
