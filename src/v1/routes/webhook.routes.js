33bc6e9931f5663322248256ecb0a86d41ece3d7
PeaceOloruntoba <profprincepeace@gmail.com>
2025-08-12 22:13:56 +0100

    Update webhook.routes.js

--- src/v1/routes/webhook.routes.js
+++ src/v1/routes/webhook.routes.js
@@ -8,25 +8,28 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const router = express.Router();
 
-router.post("/", express.raw({ type: "application/json" }), async (req, res) => {
-  const sig = req.headers["stripe-signature"];
-  let event;
+router.post(
+  "/",
+  express.raw({ type: "application/json" }),
+  async (req, res) => {
+    const sig = req.headers["stripe-signature"];
+    let event;
 
-  try {
-    event = stripe.webhooks.constructEvent(
-      req.body,
-      sig,
-      process.env.STRIPE_WEBHOOK_SECRET
-    );
-  } catch (err) {
-    console.error("Webhook signature verification failed.", err.message);
-    return res.status(400).send(`Webhook Error: ${err.message}`);
+    try {
+      event = stripe.webhooks.constructEvent(
+        req.body,
+        sig,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      console.error("Webhook signature verification failed.", err.message);
+      return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    await subscriptionService.handleStripeWebhook(event);
+
+    res.json({ received: true });
   }
-
-  // Handle the event
-  await subscriptionService.handleStripeWebhook(event);
-  
-  res.json({ received: true });
-});
+);
 
 export default router;
