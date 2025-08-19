d7cf7e15b5ca070681b0e11b9856932b99e0d9e1
PeaceOloruntoba <profprincepeace@gmail.com>
2025-08-19 01:06:39 +0100

    cloud fix

--- src/v1/services/upload.service.js
+++ src/v1/services/upload.service.js
@@ -2,7 +2,6 @@ import cloudinary from "../../lib/cloudinary.config.js";
 import ApiError from "../../utils/apiError.js";
 
 export const uploadToCloudinary = async (tempFilePath) => {
-  
   try {
     const { secure_url } = await cloudinary.v2.uploader.upload(tempFilePath, {
       use_filename: true,
@@ -10,6 +9,13 @@ export const uploadToCloudinary = async (tempFilePath) => {
     });
     return secure_url;
   } catch (error) {
+    console.error("Error updating user image:", error);
+
+    // Check if the error is already an instance of ApiError
+    if (error instanceof ApiError) {
+      throw error; // If it's an ApiError, just re-throw it
+    }
+
     throw new ApiError.internalServerError(
       500,
       "Error uploading to Cloudinary: " + error.message
